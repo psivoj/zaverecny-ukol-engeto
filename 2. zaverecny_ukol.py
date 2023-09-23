8d37e04382b7ce44c8de63a0bb770dcb345849c1
Magdalena Slanska <magdalena@slansti.cz>
2023-09-23 16:23:38 +0200

    User input and check

--- 2. zaverecny_ukol.py
+++ 2. zaverecny_ukol.py
@@ -6,6 +6,9 @@ email: magdalena@slansti.cz
 discord: magdalena2586
 """
 
+import random
+
+
 def hello():
     print("Hi there!")
     print("-----------------------------------------------")
@@ -14,4 +17,40 @@ def hello():
     print("-----------------------------------------------")
     print("Enter a number:")
 
+
+def random_number():
+    while True:
+        # Generate a random permutation of digits 0-9 and select the first 4 digits
+        digits = list(range(10))
+        random.shuffle(digits)
+        random_number = int("".join(map(str, digits[:4])))
+
+        # Ensure the number doesn't start with 0
+        if random_number >= 1000:
+            return random_number
+
+
 hello()
+
+# construct 4-digit random number not starting 0
+secret = str(random_number())
+print(secret)
+
+found = False
+while not found:
+    user_input = input(">>> ")
+    try:
+        if len(user_input) != 4:
+            raise Exception("Input must be 4 digits")
+        if user_input[0] == "0":
+            raise Exception("Input must not start with 0")
+        
+        unique_chars = set()
+        for char in user_input:
+            if char <= "0" or char >= "9":
+                raise Exception("Input must be digits only")
+            unique_chars.add(char)
+        if len(unique_chars) != 4:
+            raise Exception("Input must contain unique digits")
+    except Exception as e:
+        print(e)
