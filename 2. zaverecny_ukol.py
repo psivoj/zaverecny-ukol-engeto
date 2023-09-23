e70aaaf1c48e520c62b6d80f4a466d03bafb8dc8
Magdalena Slanska <magdalena@slansti.cz>
2023-09-23 15:54:49 +0200

    Hello to the player

--- 2. zaverecny_ukol.py
+++ 2. zaverecny_ukol.py
@@ -1,8 +1,17 @@
 """
-2.zaverecny_ukol.py: druhý projekt do Engeto Online Python Akademie
+2.zaverecny_ukol.py: druhý projekt do Engeto Online Python Akademie - Bulls and Cows
 
 author: Magdalena Slánská
 email: magdalena@slansti.cz
 discord: magdalena2586
 """
 
+def hello():
+    print("Hi there!")
+    print("-----------------------------------------------")
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    print("-----------------------------------------------")
+    print("Enter a number:")
+
+hello()
