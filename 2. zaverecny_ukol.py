1b80d1eb2bb3dd0427ca081adf39fb4d439668f2
Magdalena Slanska <magdalena@slansti.cz>
2023-09-23 16:36:46 +0200

    Guessing and evalutation

--- 2. zaverecny_ukol.py
+++ 2. zaverecny_ukol.py
@@ -30,13 +30,19 @@ def random_number():
             return random_number
 
 
+def pluralize(str, suffix, count):
+    if count == 1:
+        return str
+    return str + suffix
+
+
 hello()
 
 # construct 4-digit random number not starting 0
 secret = str(random_number())
-print(secret)
 
 found = False
+guesses = 0
 while not found:
     user_input = input(">>> ")
     try:
@@ -44,13 +50,36 @@ while not found:
             raise Exception("Input must be 4 digits")
         if user_input[0] == "0":
             raise Exception("Input must not start with 0")
-        
+
         unique_chars = set()
         for char in user_input:
-            if char <= "0" or char >= "9":
+            if char < "0" or char > "9":
                 raise Exception("Input must be digits only")
             unique_chars.add(char)
         if len(unique_chars) != 4:
             raise Exception("Input must contain unique digits")
     except Exception as e:
         print(e)
+        continue
+
+    guesses += 1
+    bulls = 0
+    cows = 0
+    for pos in range(4):
+        if secret[pos] == user_input[pos]:
+            bulls += 1
+        else:
+            if secret.find(user_input[pos]) >= 0:
+                cows += 1
+
+    if bulls == 4:
+        print("Correct, you've guessed the right number")
+        print("in", guesses, pluralize("guess", "es", guesses) + "!")
+        if guesses < 10:
+            print("That's amazing")
+        elif guesses < 20:
+            print("That's average")
+        else:
+            print("That's below expectations")
+        quit()
+    print(bulls, pluralize("bull", "s", bulls) + ",", cows, pluralize("cow", "s", cows))
